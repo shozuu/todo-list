f57dd35ca65835b88c26bf61b56aff023f64ef38
Allen Tan <tanallenbernard@gmail.com>
2024-06-21 13:26:20 +0800

    create function to dynamically render created project objects

--- src/modules/domManipulation.js
+++ src/modules/domManipulation.js
@@ -1,4 +1,6 @@
 import { dateFormatter, getToday, getTom, getMin } from "./dateHandler";
+import { setImgs, imgObjects } from "./imageHandler";
+import { getProjects } from "./projects";
 
 export function createElement(elementType, elementClass = [], elementAttribute = {}) {
     const element = document.createElement(elementType);
@@ -59,4 +61,25 @@ export function setDefault() {
     const datePicker = document.querySelector('.date-picker');
     datePicker.setAttribute('value', getMin());
     setDuePlaceholder(getToday());
+}
+
+export function renderProjects() {
+    const projects = getProjects();
+    const projectGroup = document.querySelector('.project-group');
+    const addProjectPopup = document.querySelector('.add-project-popup');
+    const navProjects = document.querySelectorAll('.nav-projects');
+    navProjects.forEach(projects => {
+        projects.remove();
+    });
+
+    Object.keys(projects).forEach(key => {
+        const navProjects = createElement('div', ['nav-projects']);
+        const projectImg = createElement('img', ['projectImg'], {src: "", alt: 'Project'});
+        const name = createElement('div', ['name'], {textContent: key});
+        const count = createElement('div', ['count'], {textContent: 0});
+        appendElement(navProjects, [projectImg, name, count]);
+        projectGroup.insertBefore(navProjects, addProjectPopup);
+    });
+
+    setImgs(imgObjects);
 }
