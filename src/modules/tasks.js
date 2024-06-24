ee4b404c1abf263aeb86d7cde0fc7b950dbbb945
Allen Tan <tanallenbernard@gmail.com>
2024-06-24 08:53:59 +0800

    create tasks object and factory function to generate objs

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,3 +1,13 @@
-function test() {
-    console.log()
+export const tasks = {};
+
+export function createTask(taskTitle, taskDesc, taskPriority, taskDue, taskProject) {
+    return {
+        taskTitle, 
+        taskDesc, 
+        taskPriority, 
+        taskDue, 
+        taskProject
+    }
 }
+
+//maybe create a function for every retrieval? like for today, tommorrow, all the completed, etc
