85599d684ba3ebfa6b12ed133f97f822c5862feb
Allen Tan <tanallenbernard@gmail.com>
2024-06-26 14:53:30 +0800

    create default sample tasks use

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,4 +1,26 @@
-export const tasks = {};
+export const tasks = {
+    'sample task': {
+        taskDesc: 'description',
+        taskDue: '2024-06-25',
+        taskPriority: 'Low Priority',
+        taskProject: 'Default',
+        taskTitle: 'sample task'
+    },
+    'sample task2': {
+        taskDesc: 'description2',
+        taskDue: '2024-06-26',
+        taskPriority: 'Medium Priority',
+        taskProject: 'Default',
+        taskTitle: 'sample task2'
+    },
+    'sample task3': {
+        taskDesc: 'description3',
+        taskDue: '2024-06-27',
+        taskPriority: 'High Priority',
+        taskProject: 'Default',
+        taskTitle: 'sample task3'
+    },
+};
 
 export function createTask(taskTitle, taskDesc, taskPriority, taskDue, taskProject) {
     return {
