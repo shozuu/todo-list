bfc8b5333a825b4de99863c0d6079c9171ebbf80
Allen Tan <tanallenbernard@gmail.com>
2024-06-28 20:38:53 +0800

    create getWeekRange function to check if taskDue is within current week

--- src/modules/dateHandler.js
+++ src/modules/dateHandler.js
@@ -1,4 +1,4 @@
-import { format, add } from 'date-fns';
+import { format, add, startOfWeek, endOfWeek, parse, isWithinInterval } from 'date-fns';
 
 export function dateFormatter(value) {
     return format(value, 'EEE, dd MMM');
@@ -6,7 +6,7 @@ export function dateFormatter(value) {
 
 export function getToday() { //no need to format
     const today = new Date();
-    return format(today, 'EEE, dd MMM');
+    return format(today, 'yyyy-MM-dd');
 }
 
 export function getMin() { //need to be passed to dateFormatter
@@ -18,4 +18,19 @@ export function getTom() { //need to be passed to dateFormatter
     const today = new Date();
     const tomorrow = add(today, { days: 1 });
     return format(tomorrow, 'yyyy-MM-dd');
+}
+
+export function getWeekRange(value) {
+    const today = new Date(); 
+    
+    //starts on monday (change to 0 if sunday)
+    const weekStart = startOfWeek(today, { weekStartsOn: 1 })
+    const weekEnd = endOfWeek(today, { weekStartsOn: 1 })
+
+    //convert string 'value' to Date object
+    const checkDate = parse(value, 'yyyy-MM-dd', new Date());
+
+    const isInThisWeek = isWithinInterval(checkDate, { start: weekStart, end: weekEnd })
+
+    return isInThisWeek;
 }
