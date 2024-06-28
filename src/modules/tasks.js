a5892bc13724c15d6cbd81b849e53a592ed4a2eb
Allen Tan <tanallenbernard@gmail.com>
2024-06-28 20:44:23 +0800

    create sample tasks to render

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,25 +1,40 @@
 export const tasks = {
     'sample task': {
         taskDesc: 'description',
-        taskDue: '2024-06-25',
+        taskDue: '2024-06-26',
         taskPriority: 'Low Priority',
         taskProject: 'Default',
         taskTitle: 'sample task'
     },
     'sample task2': {
         taskDesc: 'description2',
-        taskDue: '2024-06-26',
+        taskDue: '2024-06-27',
         taskPriority: 'Medium Priority',
         taskProject: 'Default',
         taskTitle: 'sample task2'
     },
     'sample task3': {
         taskDesc: 'description3',
-        taskDue: '2024-06-27',
+        taskDue: '2024-06-28',
         taskPriority: 'High Priority',
         taskProject: 'Default',
         taskTitle: 'sample task3'
     },
+    'sample task4': {
+        taskDesc: 'description4',
+        taskDue: '2024-06-28',
+        taskPriority: 'Medium Priority',
+        taskProject: 'Sample',
+        taskTitle: 'sample task4'
+    },
+    'sample task5': {
+        taskDesc: 'description5',
+        taskDue: '2024-06-29',
+        taskPriority: 'High Priority',
+        taskProject: 'Sample',
+        taskTitle: 'sample task5'
+    },
+
 };
 
 export function createTask(taskTitle, taskDesc, taskPriority, taskDue, taskProject) {
