b3928b0a1a0cf5c5649a62d41c368bbe43175792
Allen Tan <tanallenbernard@gmail.com>
2024-06-29 18:13:19 +0800

    display pending tasks

--- src/modules/displayTasks.js
+++ src/modules/displayTasks.js
@@ -9,11 +9,11 @@ export function getTasks(value) {
         if (value === nav) {
             if (value === 'Tomorrow') {
                 setTomorrow();
-                handleTaskNav(value); 
+                displayTasks(value, 'nav'); 
             }
             else {
                 setDefault();
-                handleTaskNav(value);
+                displayTasks(value, 'nav');
             }
             taskListener();
         }
@@ -22,78 +22,81 @@ export function getTasks(value) {
     projects.forEach(project => {
         if (value === project) {
             setDefault();
-            handleProjectNav(value);
+            displayTasks(value, 'project');
         }
         taskListener();
     });
 }
 
-function handleTaskNav(value) {
+function displayTasks(value, type) {
     const taskView = document.querySelector('.task-view')
     taskView.innerHTML = '';
     const tag = createElement('div', ['tag'], {'data-value': value, textContent: value});
-    const taskCounter = createTaskCounter(value);
-
-    //above are task counters
-    const taskContainer = createElement('div', ['task-container']);
-
-    switch (value) {
-        case 'Today':
-            Object.keys(tasks).forEach(key => {
-                if (tasks[key].taskDue === getToday()) {
-                    appendElement(taskContainer, [createTaskCard(tasks[key])])
-                }
-            }) 
-            break;
-        case 'Tomorrow':
-            Object.keys(tasks).forEach(key => {
-                if (tasks[key].taskDue === getTom()) {
-                    appendElement(taskContainer, [createTaskCard(tasks[key])])
-                }
-            })
-            break;
-        case 'This Week':
-            Object.keys(tasks).forEach(key => {
-                if (getWeekRange(tasks[key].taskDue)) {
-                    appendElement(taskContainer, [createTaskCard(tasks[key])])
-                }
-            })
-            break;
-        case 'Planned':
-            Object.keys(tasks).forEach(key => {
-                if (!getWeekRange(tasks[key].taskDue)) {
-                    appendElement(taskContainer, [createTaskCard(tasks[key])])
-                }
-            })
-            break;
-        case 'Completed':
-            Object.keys(tasks).forEach(key => {
-                //function call that passes all the checked value in task
-                console.log('working on completed')
-            })
-            break;
-        default:
-            break;
-    }
-
-    const addTask = createAddTask();
-    appendElement(taskView, [tag, taskCounter, taskContainer, addTask]);
-}
-
-function handleProjectNav(value) {
-    const taskView = document.querySelector('.task-view')
-    taskView.innerHTML = '';
-    const tag = createElement('div', ['tag'], {'data-value': value, textContent: value});
-    const taskCounter = createTaskCounter(value);
+    let taskCounter;
+    let pendingTaskCount = 0;
+    let completedTaskCount = 0;
 
     //above are task counters
     const taskContainer = createElement('div', ['task-container']);
     
-    Object.keys(tasks).forEach(key => {
-        if (value === tasks[key].taskProject) {
-            appendElement(taskContainer, [createTaskCard(tasks[key])])
+    if (type === 'nav') {
+        switch (value) {
+            case 'Today':
+                Object.keys(tasks).forEach(key => {
+                    if (tasks[key].taskDue === getToday()) {
+                        appendElement(taskContainer, [createTaskCard(tasks[key])])
+                        pendingTaskCount++;
+                    }
+                }) 
+                taskCounter = createTaskCounter(pendingTaskCount);
+                break;
+            case 'Tomorrow':
+                Object.keys(tasks).forEach(key => {
+                    if (tasks[key].taskDue === getTom()) {
+                        appendElement(taskContainer, [createTaskCard(tasks[key])])
+                        pendingTaskCount++;
+                    }
+                })
+                taskCounter = createTaskCounter(pendingTaskCount);
+                break;
+            case 'This Week':
+                Object.keys(tasks).forEach(key => {
+                    if (getWeekRange(tasks[key].taskDue)) {
+                        appendElement(taskContainer, [createTaskCard(tasks[key])])
+                        pendingTaskCount++;
+                    }
+                })
+                taskCounter = createTaskCounter(pendingTaskCount);
+                break;
+            case 'Planned':
+                Object.keys(tasks).forEach(key => {
+                    if (!getWeekRange(tasks[key].taskDue)) {
+                        appendElement(taskContainer, [createTaskCard(tasks[key])])
+                        pendingTaskCount++;
+                    }
+                })
+                taskCounter = createTaskCounter(pendingTaskCount);
+                break;
+            case 'Completed':
+                Object.keys(tasks).forEach(key => {
+                    //function call that passes all the checked value in task
+                    console.log('working on completed')
+                })
+                taskCounter = createTaskCounter(pendingTaskCount);
+                break;
+            default:
+                break;
         }
-    })
+    }
+    else if (type === 'project') {
+        Object.keys(tasks).forEach(key => {
+            if (value === tasks[key].taskProject) {
+                appendElement(taskContainer, [createTaskCard(tasks[key])])
+                pendingTaskCount++;
+            }
+        })
+        taskCounter = createTaskCounter(pendingTaskCount);
+    }
 
     const addTask = createAddTask();
     appendElement(taskView, [tag, taskCounter, taskContainer, addTask]);
@@ -104,7 +107,7 @@ function createTaskCounter(value) {
 
     const taskCounter = createElement('div', ['task-counter']);
     const card = createElement('div', ['card']);
-    const toComplete = createElement('div', ['toComplete'], {textContent: '0'});
+    const toComplete = createElement('div', ['toComplete'], {textContent: value});
     const cardName = createElement('div', ['card-name'], {textContent: 'Tasks to be Completed'});
     const card2 = createElement('div', ['card']);
     const completed = createElement('div', ['completed'], {textContent: '0'}); //should be dynamic value
