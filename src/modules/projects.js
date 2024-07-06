6840f93cbe44fcf2badc001f1300789f35944427
Allen Tan <tanallenbernard@gmail.com>
2024-07-06 22:29:23 +0800

    create function to handle renaming projects

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,4 +1,6 @@
+import { getTasks } from "./displayTasks";
 import { renderProjects } from "./domManipulation";
+import { tasks } from "./tasks";
 
 export const projects = ['Default', 'Sample']
 
@@ -17,4 +19,30 @@ export function createProject(projectTitle) {
     if (flag) return;
     projects.push(projectTitle);
     renderProjects();
+}
+
+export function renameProj(oldProjectTitle, newProjectTitle) {
+    let indexToRemove;
+
+    for (let i = 0; i < projects.length; i++) {
+        if (projects[i].toLowerCase() === newProjectTitle.toLowerCase()) {
+            alert(`Project ${newProjectTitle} already exists`)
+            return;
+        }
+        if (projects[i] === oldProjectTitle) {
+            indexToRemove = i;
+        }
+    }
+
+    projects.splice(indexToRemove, 1, newProjectTitle);
+
+    Object.keys(tasks).forEach(task => {
+        if (tasks[task].taskProject === oldProjectTitle) {
+            tasks[task].taskProject = newProjectTitle;
+        }
+        console.log(tasks[task])
+    })
+
+    renderProjects();
+    getTasks(newProjectTitle);
 }
