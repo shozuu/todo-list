4fa17c32f823b0f8617ac27f86ccb45f1b52b4a6
Allen Tan <tanallenbernard@gmail.com>
2024-07-11 19:17:49 +0800

    create new module to handle functions for responsiveness

---
+++ src/modules/mediaQuery.js
@@ -0,0 +1,28 @@
+export function initializeMediaQuery() {
+    const mediaQuery = window.matchMedia('(min-width: 770px)');
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery); //initial run to check the current width
+}
+
+function handleMediaQueryChange(event) {
+    const content = document.querySelector('.content');
+    const contentBackdrop = document.querySelector('.content-backdrop');
+    const sidebar = document.querySelector('.sidebar');
+    const secSidebarButton = document.querySelector('.second-sidebar-icon');
+
+    if (event.matches) { //more than 770px
+        if (!sidebar.classList.contains('hidden')) { //if sidebar is active
+            content.classList.add('inactive');
+            contentBackdrop.classList.add('hidden'); //enclose in if
+        }
+    } 
+    else { //less than 770px
+        content.classList.remove('inactive');
+        sidebar.classList.add('hidden');
+        secSidebarButton.classList.remove('hidden');
+    }
+}
+
+//min-width: 770 = true if 770 or more && false if below 770
+//max-width: 770 = true if 770 or less && false if more than 770
